8a4d2e7c24c31c02a162f0b4af5c8bb169fae098
William Smith <richardsmithortegon@gmail.com>
2019-04-11 10:52:12 -0500

    Canje-get

--- src/app/canje/canje-get/canje-get.component.ts
+++ src/app/canje/canje-get/canje-get.component.ts
@@ -1,5 +1,7 @@
+import { CanjeService } from './../canje.service';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { CanjeDetail } from '../canjeDetail';
 @Component({
   selector: 'app-canje-get',
   templateUrl: './canje-get.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanjeGetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private canjeService: CanjeService,
+    private route: ActivatedRoute) { }
 
-  ngOnInit() {
+    canjeId: number;
+    canje: CanjeDetail;   
+    /**
+     * Le pide al servicio el usuario
+     */
+    getCanje(): void{
+        this.canjeService.getCanjeDetail(this.canjeId).subscribe(c => this.canje = c);
+    }
+
+    ngOnInit(){
+      this.canjeId = + this.route.snapshot.paramMap.get('id');
+      this.getCanje();
+  
   }
 
 }
