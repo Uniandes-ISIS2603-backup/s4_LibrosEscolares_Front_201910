810690231062c5242fed6089884663c105b0cdff
ma.munoza <ma.munoza@uniandes.edu.co>
2019-04-26 12:08:11 -0500

    comienzo libros-edit

---
+++ src/app/libros/libros-edit/libros-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-libros-edit',
+  templateUrl: './libros-edit.component.html',
+  styleUrls: ['./libros-edit.component.css']
+})
+export class LibrosEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
